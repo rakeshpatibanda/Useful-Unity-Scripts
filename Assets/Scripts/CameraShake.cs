3bbda7dc1750d8372a49859aabed232a0d06add9
Rakesh Patibanda <rakesh.patibanda@rmit.edu.au>
2019-07-01 23:48:15 +1000

    Useful Unity Scripts
    Useful Unity Scripts

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // VARIABLE TO ACCESS THE TRANSFORM OF THE CAMERA
+    Transform camTrans;
+
+    // VARIABLE TO DECIDE HOW MUCH TIME THE CAMERA SHAKE SHOULD HAPPEN
+    public float shakeTime;
+
+    // VARIABLE TO DECIDE UPTO HOW MUCH RANGE WE WANT TO MOVE THE CAMERA I.E., IF THE RANGE IS SET TO 5
+    // THE CAMERA CAN MOVE IN THE X, Y AND Z AXIS FOR 5 TIMES
+    public float shakeRange;
+
+    // ORIGINAL POSITION OF THE CAMERA
+    Vector3 originalPositionOfCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GETTING THE TRANSFORM ACCESS OF THE CAMERA AND ASSIGNING IT TO @camTrans
+        camTrans = Camera.main.transform; 
+
+        // GETTING THE POSITION VALUE OF THE CAMERA  
+        originalPositionOfCamera = camTrans.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+
+            // STARTING THE COROUTINE
+            StartCoroutine(ShakeCamera());
+        }
+    }
+
+    IEnumerator ShakeCamera(){
+
+        float timeElapsed = 0;
+
+        while(timeElapsed < shakeTime)
+        {
+            // CHANGING THE ORIGINAL POSITION OF THE CAMERA BY ADDING IT TO THE RANDOM SHAKE VECTOR3 VARIABLE
+            // CREATED BY INSIDEUNITSPHERE MUTIPLIED BY SHAKERANGE
+            Vector3 tempCamPos =  originalPositionOfCamera + Random.insideUnitSphere * shakeRange;
+
+            // ASSIGNING TEMPCAMPPOS Z AXIS VALUE TO THE Z VALUE OF THE ORIGINAL CAM POSITION TO AVOID
+            // SHAKING IT IN THE Z AXIS AND AVOID THE WEIRD ZOOM IN AND ZOOM OUT EFFECT
+            tempCamPos.z = originalPositionOfCamera.z;
+
+            // WE WILL THEN REASSIGN THE NEW VALUE OF TEMPCAMPPOS TO THE CAMTRANS POSITION
+            camTrans.position = Vector3.Lerp(originalPositionOfCamera, tempCamPos, shakeTime);
+            
+            // INCREMENT THE TIME (ELSE IT WILL CONTINUOUSLY LOOP AND THE LOOP WILL NEVER END)
+            timeElapsed += Time.deltaTime;
+
+            // MOST IMPORTANT STATEMENT IN A COROUTINE FUNCTION - IT WILL HALT THE EXECUTION OF THE 
+            // WHILE LOOP UNTIL THE END OF THE CURRENT FRAME
+            yield return null;
+        }
+
+        camTrans.position = originalPositionOfCamera;
+
+    }
+}
---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{   
+    // VARIABLES FOR MIN AND MAX ZOOM FOR 2D CAMERA
+    public float minZoom2D, maxZoom2D;
+
+    // VARIABLES FOR MIN AND MAX ZOOM FOR 3D CAMERA
+    public float minZoom3D, maxZoom3D;
+
+    // VARIABLE TO SET ZOOM SPEED FOR 2D CAMERA
+    public float zoomSpeed2D;
+
+    // VARIABLE TO SET ZOOM SPEED FOR 3D CAMERA
+    public float zoomSpeed3D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // CHECK FOR 2D CAMERA SCROLL MOVEMENT
+        if(Camera.main.orthographic)
+        {
+            CameraZoom2D();
+        }else
+        {
+            CameraZoom3D();
+        }
+        
+    }
+
+    // 2D CAMERA ZOOM FUNCTION
+    void CameraZoom2D(){
+        // CHECK IF THE SCROLL WHEEL IS MOVING UPWARDS
+            if(Input.GetAxis("Mouse ScrollWheel") > 0){
+                // ZOOM IN
+                Camera.main.orthographicSize -= zoomSpeed2D * Time.deltaTime;
+            }
+            else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                // ZOOM OUT
+                Camera.main.orthographicSize += zoomSpeed2D * Time.deltaTime;
+            } 
+
+            // RESTRICTING THE MIN AND MAX ZOOM VALUE WHEN 2D CAMERA
+            Camera.main.orthographicSize =  Mathf.Clamp(Camera.main.orthographicSize, minZoom2D, maxZoom2D);
+    }
+
+    // 3D CAMERA ZOOM FUNCTION
+    void CameraZoom3D(){
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            // ZOOM IN
+            Camera.main.fieldOfView -= zoomSpeed3D * Time.deltaTime;
+        } 
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            // ZOOM OUT
+            Camera.main.fieldOfView += zoomSpeed3D * Time.deltaTime;
+        }
+
+        // RESTRICTING THE MIN AND MAX ZOOM VALUE WHEN 3D CAMERA
+        Camera.main.fieldOfView =  Mathf.Clamp(Camera.main.fieldOfView, minZoom3D, maxZoom3D);
+    }
+}
