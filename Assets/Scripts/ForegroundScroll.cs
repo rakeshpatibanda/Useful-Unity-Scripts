3582b9fc72b1b972398409fab02de38d5cea255b
Rakesh Patibanda <rakesh.patibanda@rmit.edu.au>
2019-07-17 21:50:35 +1000

    a lot of useful unity scripts

---
+++ Assets/Scripts/ForegroundScroll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForegroundScroll : MonoBehaviour
+{
+    // GET ACCESS TO THE MATERIAL OF THIS PARTICULAR OBJECT
+    Material material;
+
+    // NEW VECTOR2 VARIABLE TO SET OFFSET VALUE
+    Vector2 offset;
+
+    // INT VARIBALES FOR SETTING THE VALUES OF THE MAINTEXTUREOFFSET OF THIS OBJECT
+    public int xVelocity;
+    public int yVelocity;
+
+
+    public void Awake() {
+        // WHEN UNITY STARTS ASSIGN THE MATERIAL VALUE TO THE MATERIAL VALUE OF THIS FOREGROUND OBJECT
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setting the new offset value to time
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/RotateWithMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour
+{
+
+    // VARIABLE TO DETERMINE THE SPEED OF THE ROTATION 
+    public float rotationSpeed;
+    private void OnMouseDrag() 
+    {   
+       // ASSGINING THE VALUE OF THE ROTATED X AXIS MULTIPLIED  BY THE SET ROTATIONSPEED TO A
+       // TEMPORARY FLOAT X 
+       float x = Input.GetAxis("Mouse X") * rotationSpeed;
+       float y = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+       // ONCE WE GET THE X VALUE WE NEED TO ROTATE THE OBJECT ACCORDINGLY
+       transform.Rotate(Vector3.up, -x);
+
+       // ONCE WE GET THE Y VALUE WE NEED TO ROTATE THE OBJECT ACCORDINGLY
+       transform.Rotate(Vector3.left, -y);
+       
+
+    }
+}
